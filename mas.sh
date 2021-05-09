5b8cacc53fcf884ce59ad2e8957f209558062702
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2021-05-08 22:48:09 -0700

    Add Mas

---
+++ mas.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Install Mac App Store apps using mas-cli
+# https://github.com/mas-cli/mas
+
+#brew install mas
+
+# Install apps
+mas install 445189367 #PopClip
+open /Applications/PopClip.app
+mas install 417375580 #BetterSnapTool
+open /Applications/BetterSnapTool.app
+mas install 937984704 #Amphetamine
+mas install 1176895641 #Spark Mail 
+mas install 975937182 #Fantastical
+mas install 1055511498 #Day One
+mas install 414209656 #Better Rename 9
+mas install 360593530 #Notability
+mas install 1402042596 #Adblock Extension
+mas install 425424353 #The Unarchiver
+mas install 904280696 #Things
+mas install 1438243180 #Dark Reader for Safari 
+
+# mas install 409201541 #Pages
+# mas install 409203825 #Numbers
+# mas install 409183694 #Keynote
