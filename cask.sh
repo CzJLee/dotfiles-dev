850add5a653d6e378cf9613b90642dc888954a2b
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2021-05-08 22:42:29 -0700

    Add cask

---
+++ cask.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Install Mac Apps using Homebrew Cask
+# https://github.com/Homebrew/homebrew-cask
+
+# Check if an app is avaliable by typing "brew search AppName"
+
+# Apps with licenses
+brew install --cask 1password #Need online version and activation key in order to work
+brew install --cask coderunner #online version has more capabilities 
+brew install --cask bartender
+brew install --cask daisydisk
+brew install --cask tg-pro
+
+
+# Install Mac Apps
+brew install --cask adobe-acrobat-reader
+brew install --cask appcleaner
+brew install --cask duet
+brew install --cask flux
+brew install --cask google-chrome
+brew install --cask imageoptim
+brew install --cask visual-studio-code
+# brew install --cask zoom
+brew install --cask transmit
+brew install --cask mongodb-compass
+brew install --cask postman
