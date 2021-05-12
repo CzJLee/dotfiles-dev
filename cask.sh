84fab354abb9aa53f4f151239ee9f552ea7f6116
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2021-05-12 16:08:02 -0700

    Edit casks

--- cask.sh
+++ cask.sh
@@ -8,16 +8,16 @@
 # Apps with licenses
 brew install --cask 1password #Need online version and activation key in order to work
 brew install --cask coderunner #online version has more capabilities 
-brew install --cask bartender
+# brew install --cask bartender # Bartender 3 does not work on big sur
 brew install --cask daisydisk
 brew install --cask tg-pro
 
 
 # Install Mac Apps
-brew install --cask adobe-acrobat-reader
+# brew install --cask adobe-acrobat-reader
 brew install --cask appcleaner
-brew install --cask duet
-brew install --cask flux
+# brew install --cask duet
+# brew install --cask flux
 brew install --cask google-chrome
 brew install --cask imageoptim
 brew install --cask visual-studio-code
