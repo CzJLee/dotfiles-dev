4b4673858852dce4ece8ae155681b18fb0b7ed9f
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2021-05-14 07:56:50 -0700

    Add Infuse

--- mas.sh
+++ mas.sh
@@ -11,7 +11,7 @@ open /Applications/PopClip.app
 mas install 417375580 #BetterSnapTool
 open /Applications/BetterSnapTool.app
 mas install 937984704 #Amphetamine
-mas install 1176895641 #Spark Mail 
+mas install 1176895641 #Spark Mail
 mas install 975937182 #Fantastical
 mas install 1055511498 #Day One
 mas install 414209656 #Better Rename 9
@@ -20,6 +20,7 @@ mas install 1402042596 #Adblock Extension
 mas install 425424353 #The Unarchiver
 mas install 904280696 #Things
 mas install 1438243180 #Dark Reader for Safari 
+mas install 1136220934 #Infuse 7
 
 # mas install 409201541 #Pages
 # mas install 409203825 #Numbers
